bc464a82dababf9b0c75b429fdb0cf4981ca13af
Reggie <jiwookim5121@gmail.com>
2026-04-14 05:29:51 +0900

    Update 18234.py

--- 26-1 스터디/2주차/18234.py
+++ 26-1 스터디/2주차/18234.py
@@ -1,1 +1,15 @@
+import sys
+input = sys.stdin.readline
 
+N, T = map(int, input().split())
+carrots = [tuple(map(int, input().split())) for _ in range(N)]
+
+carrots.sort(key=lambda x: x[1])
+
+answer = 0
+
+for i in range(N):
+    w, p = carrots[i]
+    answer += w + (T - N + i) * p
+
+print(answer)
