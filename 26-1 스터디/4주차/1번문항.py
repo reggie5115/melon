bb3fc576f43cf8e5bd2cfdf83117f5b069064032
Reggie <jiwookim5121@gmail.com>
2026-05-12 11:20:18 +0900

    Update 1번문항.py

--- 26-1 스터디/4주차/1번문항.py
+++ 26-1 스터디/4주차/1번문항.py
@@ -4,12 +4,15 @@ def solution(s):
     answer = []
     last_index = {}
 
+    # 문자열을 한 글자씩 확인
     for i, ch in enumerate(s):
-        if ch in last_index:
-            answer.append(i - last_index[ch])
+
+        if ch in last_index:  # 이전에 같은 문자가 나온 적이 있다면
+            answer.append(i - last_index[ch])  # 현재 위치 - 이전 위치 = 거리
         else:
             answer.append(-1)
 
+        # 현재 문자의 위치를 최신 위치로 갱신
         last_index[ch] = i
 
     return answer
